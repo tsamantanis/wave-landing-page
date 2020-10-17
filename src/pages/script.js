b90b3b439454334e0d8bce2d7af3a28abcc898a6
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-16 17:46:32 -0700

    Finalize choose what you see illustration animation

--- src/pages/script.js
+++ src/pages/script.js
@@ -2,18 +2,20 @@ console.log("huu");
 setTimeout(function() {
     let paths = [
         document.getElementById('ChooseWhatYouSeeIllustration_1'),
+        document.getElementById('ChooseWhatYouSeeIllustration_2'),
     ]
-    paths.forEach((path) => {
-        let pathLength = path.getTotalLength();
-        path.style.strokeDasharray = pathLength + ' ' + pathLength;
-        path.style.strokeDashoffset = pathLength;
-        path.getBoundingClientRect();
-        window.addEventListener("scroll", function(e) {
+
+    window.addEventListener("scroll", function(e) {
+        paths.forEach((path) => {
+            let pathLength = path.getTotalLength();
+            path.style.strokeDasharray = pathLength + ' ' + pathLength;
+            path.style.strokeDashoffset = pathLength;
+            path.getBoundingClientRect();
             let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-            if (scrollPercentage > 0.56) {
-                let drawLength = pathLength * (0.56 - scrollPercentage) * 5;
+            if (scrollPercentage > 0.55) {
+                let drawLength = pathLength * (0.55 - scrollPercentage) * 5;
                 path.style.strokeDashoffset = pathLength - drawLength;
-                if (scrollPercentage >= 0.72) {
+                if (scrollPercentage >= 0.73) {
                     path.style.strokeDasharray = "none";
                 } else {
                     path.style.strokeDasharray = pathLength + ' ' + pathLength;
