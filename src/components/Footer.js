c8c9f72316f93bd4e128f139fe6702be89660bdf
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-19 13:51:19 -0700

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = ({ children }) => {
+    return (
+        <footer className="text-center align-items-center d-flex justify-content-center">
+            <p>Copyright 2020 @ Philippos Tsamantanis</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Navigation from './Navigation';
+import Footer from './Footer';
 
 import '../assets/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
         <>
             <Navigation />
             {children}
+            <Footer />
         </>
     );
 };
