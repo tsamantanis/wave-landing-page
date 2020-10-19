a2e683d77dae3e7f8a6a1f3f82e0cbe66cd1051c
tsamantanis <philip.tsamantanis@icloud.com>
2020-10-19 11:59:52 -0700

    Fix typo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Navigation from './navigation';
+import Navigation from './Navigation';
 
 import '../assets/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
